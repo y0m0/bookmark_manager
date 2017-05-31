e7dbe0ea4cd0779f892d57fac2f84e7677e15767
y0m0 <lixi.daniele@gmail.com>
2017-05-31 15:10:19 +0100

    Adds views/links/index - returns an unordered list with 1 element for each link in our db - pass feature test to see links

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -1,6 +1,15 @@
 require 'sinatra/base'
-require_relative 'modules/link'
+require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
+  
+  get '/' do
+    'Bookmark Manager'
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
 
 end
--- spec/features/view_links_spec.rb
+++ spec/features/view_links_spec.rb
@@ -4,8 +4,12 @@ feature 'view links' do
   scenario 'I want to see a list of links' do
     Link.create(url: "http://hackaday.com/", title: "Hackaday")
     visit '/links'
-    # to check if this works
-    # within 'ul#links' do
-    expect(page).to have_content('Hackaday')
+
+    # check if the page is loaded correctly
+    expect(page.status_code).to eq 200
+
+    within 'ul#links' do
+      expect(page).to have_content('Hackaday')
+    end
   end
 end
