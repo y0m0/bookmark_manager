36289b34205d1d32cfac814fc1bc7980a575c1f1
y0m0 <lixi.daniele@gmail.com>
2017-05-31 22:05:17 +0100

    Adds Heroku postresql support

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,7 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 
 class Link
   include DataMapper::Resource
