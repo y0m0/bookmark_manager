cabea2c7ddd8c055604cd409005ea34f4b133f69
y0m0 <lixi.daniele@gmail.com>
2017-06-01 18:25:24 +0100

    links/new now adds an entry into the link_tags table

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -20,7 +20,11 @@ class BookmarkManager < Sinatra::Base
   end
   
   post '/links' do
-    Link.create(url: params[:url], title: params[:title])
+    link = Link.new(url: params[:url],
+             title: params[:title])
+    tag = Tag.first_or_create(name: params[:tags])
+    link.tags << tag
+    link.save
     redirect '/links'
   end
 
